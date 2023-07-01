24bf1b10f61c5f41bd4ba202519809c69a35823b
krist <82980687+Cris-Daiki@users.noreply.github.com>
2023-06-30 21:37:49 -0400

    ultimos cambios

---
+++ Assets/Scripts/CambiarLvl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarLvl : MonoBehaviour
+{
+    //public String Layer;
+    GameObject Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    // if(GameObject.FindGameObjectWithTag("EnemyBoss") == Null){
+    //     SceneManager.LoadScene("nivel 3");
+    // }
+    }
+}
